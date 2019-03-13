05658670c19fd8d54c8c2a44b2d5088448e4bd70
adhie <adhie@inf.elte.hu>
2019-03-13 18:06:33 +0100

    Added normalization and whole number format

--- 010_Programming_in_scala_3rd_edition/006_Chaper_5_Functional_objects/Rational.scala
+++ 010_Programming_in_scala_3rd_edition/006_Chaper_5_Functional_objects/Rational.scala
@@ -1,7 +1,15 @@
 class Rational(numerator: Int, denominator: Int) {
-  val n = numerator 
-  val d = denominator
+  val g = gcd(numerator.abs, denominator.abs)
+  val n = numerator / g
+  val d = denominator / g
   val isFinite = d != 0
+  val isWhole = d == 1
 
-  override def toString =  if (isFinite) s"$n/$d" else "Infinity"
+  override def toString = 
+    if (isWhole) n.toString
+    else if (isFinite) s"$n/$d" 
+    else "Infinity"
+
+  private def gcd(a: Int, b: Int): Int = 
+    if (b == 0) a else gcd(b, a % b)
 }
