aefcd50dafa9ddc94ee4ca88ca665caedb70f763
adhie <adhie@inf.elte.hu>
2019-03-13 18:47:30 +0100

    Added multiplication and equality

--- 010_Programming_in_scala_3rd_edition/006_Chaper_5_Functional_objects/Rational.scala
+++ 010_Programming_in_scala_3rd_edition/006_Chaper_5_Functional_objects/Rational.scala
@@ -7,7 +7,7 @@ class Rational(numerator: Int, denominator: Int) {
 
   def this(numerator: Int) = this(numerator, 1)
 
-  def add(that: Rational): Rational =
+  def add(that: Rational) =
     if (!isFinite) this
     else if (!that.isFinite) that 
     else new Rational(
@@ -15,6 +15,21 @@ class Rational(numerator: Int, denominator: Int) {
       d * that.d)
 
   def + = add(_)
+
+  def multiply(that: Rational) =
+    if (!isFinite) this
+    else if (!that.isFinite) that
+    else new Rational(n * that.n, d * that.d)
+
+  def * = multiply(_)
+
+  override def equals(that: Any) =
+    that match {
+      case that:Rational => 
+        !that.isFinite && !this.isFinite ||
+        that.n == this.n && that.d == this.d
+      case _ => false
+    }
 
   override def toString = 
     if (isWhole) n.toString
--- 010_Programming_in_scala_3rd_edition/006_Chaper_5_Functional_objects/rational_examples.scala
+++ 010_Programming_in_scala_3rd_edition/006_Chaper_5_Functional_objects/rational_examples.scala
@@ -4,4 +4,10 @@ val threeQuarters = half add quarter
 val one = quarter + threeQuarters
 val two = new Rational(2)
 val twoAndAHalf = two + half
+val infinity = new Rational(1, 0)
 
+println(s"infinity == infinity + 1? It is ${infinity == infinity + one}.")
+
+val threeByEight = threeQuarters multiply half 
+
+println(s"(3/8) * 2 == 3/4? It is ${ threeByEight * two == threeQuarters }.")
