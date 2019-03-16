6dc8b780376c3eee2cdb121a734cca2905eac429
adhie <adhie@inf.elte.hu>
2019-03-16 18:04:45 +0100

    Sqrt function

---
+++ 020_Functional_Programming_Principles_in_Scala/010_Week_1/008_Sqrt/sqrt.scala
@@ -0,0 +1,13 @@
+
+def sqrt(x: Double): Double = {
+  def abs(x: Double) = if (x < 0) -x else x
+  def isGoodEnough(guess: Double) = abs(guess * guess - x) < 0.00001
+  def improve(guess: Double) = (guess + x / guess) / 2
+  def sqrtIter(guess: Double): Double = 
+    if (isGoodEnough(guess)) guess
+    else sqrtIter((guess + x / guess) / 2)
+
+  sqrtIter(1)
+}
+
+
