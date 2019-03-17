14cea2e0a6114b2c9beadb8bf644290fb8d7deeb
adhie <adhie@inf.elte.hu>
2019-03-17 19:42:21 +0100

    Product and sum currying function with currying base

---
+++ 020_Functional_Programming_Principles_in_Scala/020_Week_2/020_TailRecursiveProduct/product.scala
@@ -0,0 +1,15 @@
+
+def aggregate(start: Int, aggregator: (Int, Int)=> Int)(f: Int => Int)(a: Int, b: Int): Int = {
+  def loop(a: Int, acc: Int): Int = {
+    if (a > b) acc
+    else loop(a + 1, aggregator(acc, f(a)))
+  }
+  loop(a, start)
+}
+
+def sum: (Int => Int) => (Int, Int) => Int 
+  = aggregate(0, _ + _)
+
+def product: (Int => Int) => (Int, Int) => Int 
+  = aggregate(1, _ * _)
+
