fdcd4e185bb5d57b6fa08e68c47af572f64e5e6f
adhie <adhie@inf.elte.hu>
2019-03-17 18:51:27 +0100

    Finished Pascal triangle

--- 020_Functional_Programming_Principles_in_Scala/010_Week_1/012_Recursion_Assignment/recfun/src/main/scala/recfun/Main.scala
+++ 020_Functional_Programming_Principles_in_Scala/010_Week_1/012_Recursion_Assignment/recfun/src/main/scala/recfun/Main.scala
@@ -4,8 +4,9 @@ object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
-      for (col <- 0 to row)
+      for (col <- 0 to row) {
         print(pascal(col, row) + " ")
+      }
       println()
     }
   }
@@ -13,7 +14,19 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+
+      def nextRow(row: Array[Int]): Array[Int] =
+        Array(row.head) ++
+          row.take(row.length - 1).zip(row.tail).map(p => p._1 + p._2) ++
+          Array(row.last)
+
+      def pascal(rowNumber: Int, row: Array[Int]): Array[Int] =
+        if (rowNumber == r) row
+        else pascal(rowNumber + 1, nextRow(row))
+
+      pascal(0, Array(1))(c)
+    }
   
   /**
    * Exercise 2
