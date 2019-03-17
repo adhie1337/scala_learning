8ce2d3b1fbf00745683c4270bc321399bb365666
adhie <adhie@inf.elte.hu>
2019-03-17 17:49:02 +0100

    Made sqrt more readable

--- 020_Functional_Programming_Principles_in_Scala/010_Week_1/008_Sqrt/sqrt.scala
+++ 020_Functional_Programming_Principles_in_Scala/010_Week_1/008_Sqrt/sqrt.scala
@@ -1,8 +1,14 @@
 
 def sqrt(x: Double): Double = {
-  def abs(x: Double) = if (x < 0) -x else x
-  def isGoodEnough(guess: Double) = abs(guess * guess - x) / x < 0.00001
+
+  def abs(x: Double) = 
+    if (x < 0) -x else x
+
+  def isGoodEnough(guess: Double) = 
+    abs(guess * guess - x) / x < 0.00001
+
   def improve(guess: Double) = (guess + x / guess) / 2
+
   def sqrtIter(guess: Double): Double = 
     if (isGoodEnough(guess)) guess
     else sqrtIter((guess + x / guess) / 2)
