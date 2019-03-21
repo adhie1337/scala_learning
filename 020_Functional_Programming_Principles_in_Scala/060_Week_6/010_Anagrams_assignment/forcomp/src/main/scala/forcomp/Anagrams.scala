c815d707637035a32ac72dc7e33037205410bab1
adhie <adhie@inf.elte.hu>
2019-03-21 08:49:46 +0100

    sentenceOccurrences function

--- 020_Functional_Programming_Principles_in_Scala/060_Week_6/010_Anagrams_assignment/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ 020_Functional_Programming_Principles_in_Scala/060_Week_6/010_Anagrams_assignment/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -38,7 +38,7 @@ object Anagrams {
     ((w toLowerCase) groupBy identity  toList) map (pair => (pair._1, pair._2.length)) sorted
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.mkString(""))
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
