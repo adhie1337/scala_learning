1cb316a477bd8e617c313b5d3e0cc99b0b4610e2
adhie <adhie@inf.elte.hu>
2019-03-24 21:39:57 +0100

    2nd order polynomial solver

--- 030_Functional_Program_Design_in_Scala/040_Week_4_Calculator_Assignment/calculator/src/main/scala/calculator/Polynomial.scala
+++ 030_Functional_Program_Design_in_Scala/040_Week_4_Calculator_Assignment/calculator/src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,23 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal({
+      val bVal = b()
+      bVal * bVal - 4 * a() * c()
+    })
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal({
+      val deltaVal = delta()
+
+
+      if (deltaVal < 0)
+        Set()
+      else if (deltaVal == 0)
+        Set(-b() / (2 * a()))
+      else Set((-b() + Math.sqrt(deltaVal)) / (2 * a()), (-b() - Math.sqrt(deltaVal)) / (2 * a()))
+    })
   }
 }
