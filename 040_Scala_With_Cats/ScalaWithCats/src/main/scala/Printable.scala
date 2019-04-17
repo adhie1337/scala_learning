05c627976c199a3e427f7b95dd6a03245b8cfd47
adhie <adhie@inf.elte.hu>
2019-04-17 20:52:18 +0200

    Printing cats with syntax

--- 040_Scala_With_Cats/ScalaWithCats/src/main/scala/Printable.scala
+++ 040_Scala_With_Cats/ScalaWithCats/src/main/scala/Printable.scala
@@ -13,3 +13,11 @@ object Printable {
 
   def print[A](value: A)(implicit p: Printable[A]): Unit = println(format(value))
 }
+
+object PrintableSyntax {
+  implicit class PrintableOps[A](value: A) {
+    def format(implicit printable: Printable[A]): String = Printable format value
+
+    def print(implicit printable: Printable[A]): Unit = Printable print value
+  }
+}
--- 040_Scala_With_Cats/ScalaWithCats/src/test/scala/PrintableTests.scala
+++ 040_Scala_With_Cats/ScalaWithCats/src/test/scala/PrintableTests.scala
@@ -6,4 +6,12 @@ class PrintableTests extends org.scalatest.FunSuite {
     assert(Printable.format(cat) == "Cirmi is a 4 year-old, brown cat")
   }
 
+  test("Printing cats with syntax") {
+    import PrintableSyntax._
+
+    val cat = Cat("Cirmi", 4, "brown")
+
+    assert(cat.format == "Cirmi is a 4 year-old, brown cat")
+  }
+
 }
